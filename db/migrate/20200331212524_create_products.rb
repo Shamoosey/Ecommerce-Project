95e198930bc781bd0f68d72911f3cb7cd2b34b8e
Shamus Osler <Shamuso23@gmail.com>
2020-03-31 16:25:42 -0500

    Added product model

---
+++ db/migrate/20200331212524_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :description
+      t.string :image
+      t.string :category
+      t.decimal :price
+      t.int :stock
+
+      t.timestamps
+    end
+  end
+end
