2bcb1e5e65e8654977cba9d0bbbada5906548e8a
Shamus Osler <Shamuso23@gmail.com>
2020-04-01 16:25:44 -0500

    Added image attachment for product table

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,3 +1,12 @@
 ActiveAdmin.register Product do
-  permit_params :name, :description, :image, :category, :price, :stock
+  permit_params :name, :description, :image, :category, :price, :stock, :image
+
+  form do |f|
+    f.semantic_errors
+    f.inputs
+    f.inputs do
+      f.input :image, as: :file
+    end
+    f.actions
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
   has_many :order_product
   validates :name, :description, :image, :category, :price, :stock, presence: true
+  has_one_attached :image
 end
