b0a9e0cd78e1bd840de56d78e762e3932260571d
Shamus Osler <Shamuso23@gmail.com>
2020-04-01 16:17:56 -0500

    Removed image from product table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_01_211143) do
+ActiveRecord::Schema.define(version: 2020_04_01_211714) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -88,7 +88,6 @@ ActiveRecord::Schema.define(version: 2020_04_01_211143) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.string "description"
-    t.string "image"
     t.string "category"
     t.decimal "price"
     t.decimal "stock"
