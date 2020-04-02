2003b686e4de324f28f89f01d24bf47e652fc0a2
Shamus Osler <Shamuso23@gmail.com>
2020-04-01 19:27:16 -0500

    Added category dropdown to active admin

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,10 +1,11 @@
 ActiveAdmin.register Product do
-  permit_params :name, :description, :price, :stock, :image
+  permit_params :name, :description, :price, :stock, :image, :categories_id
 
   form do |f|
     f.semantic_errors
     f.inputs
     f.inputs do
+      f.input :categories_id, as: :select, collection: Category.all.collect { |category| [category.name, category.id] }
       f.input :image, as: :file
     end
     f.actions
