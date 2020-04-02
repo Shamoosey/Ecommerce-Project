55e000c7586e767dce171df65da3e2c1d6b97433
Shamus Osler <Shamuso23@gmail.com>
2020-04-01 19:29:48 -0500

    Removed old reference to category

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
   has_many :order_product
-  validates :name, :description, :image, :category, :price, :stock, presence: true
+  validates :name, :description, :image, :price, :stock, presence: true
   has_one_attached :image
 end
