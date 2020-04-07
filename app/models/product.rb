8a4ac9f651f98000b7127551714937ef7d68ccbf
Shamus Osler <Shamuso23@gmail.com>
2020-04-07 16:07:26 -0500

    Added pagination to products page

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,5 @@ class Product < ApplicationRecord
   has_many :order_product
   validates :name, :description, :image, :price, :stock, presence: true
   has_one_attached :image
+  paginates_per 6
 end
