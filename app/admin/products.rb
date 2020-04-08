ae0200c727dea4916e3ae50f99523b32a0041d62
Shamus Osler <Shamuso23@gmail.com>
2020-04-08 10:46:24 -0500

    Fixed name change

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register Product do
     f.semantic_errors
     f.inputs
     f.inputs do
-      f.input :categories_id, as: :select, collection: Category.all.collect { |category| [category.name, category.id] }
+      f.input :category_id, as: :select, collection: Category.all.collect { |category| [category.name, category.id] }
       f.input :image, as: :file
     end
     f.actions
