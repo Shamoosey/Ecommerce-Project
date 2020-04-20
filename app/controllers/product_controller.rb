0d2c067a0ff9d6a60e2365978badefb27c6e5a7b
Shamus Osler <Shamuso23@gmail.com>
2020-04-20 14:44:21 -0500

    Added session based shopping cart

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,4 +1,7 @@
 class ProductController < ApplicationController
+  before_action :initalize_session
+  before_action :load_cart
+
   def index
     @products = Product.all.page params[:page]
   end
@@ -9,5 +12,25 @@ class ProductController < ApplicationController
 
   def search
     @product_results = Product.joins(:category).where('products.name LIKE ? AND products.category_id LIKE ?', "%#{params[:search_term]}%", "%#{params[:category_id]}%").page params[:page]
+  end
+
+  def add_to_cart
+    id =  params[:id].to_i
+    session[:cart] << id unless session[:cart].include?(id)
+    redirect_to root_path
+  end
+
+  def remove_from_cart
+    id =  params[:id].to_i
+    session[:cart].delete(id)
+    redirect_to root_path
+  end
+
+  def load_cart
+    @cart = Product.find(session[:cart])
+  end
+
+  def initalize_session
+    session[:cart] ||= []
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   resources :product, only: :show
   root to: 'product#index'
 
+  post 'products/add_to_cart/:id', to: 'product#add_to_cart', as: 'add_to_cart'
+  delete 'products/remove_from_cart/:id', to: 'product#remove_from_cart', as: 'remove_from_cart'
+
   get 'product/id', to: 'product#show', id: /\d+/
   get 'contact', to: 'contact#index', as: 'contact'
   get 'about', to: 'about#index', as: 'about'
