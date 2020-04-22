6123740025adb4043f8184313f1bde4deb513170
Shamus Osler <Shamuso23@gmail.com>
2020-04-22 16:59:15 -0500

    Removed Customer table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_22_210707) do
+ActiveRecord::Schema.define(version: 2020_04_22_211753) do
 
   create_table "abouts", force: :cascade do |t|
     t.string "title"
@@ -77,19 +77,6 @@ ActiveRecord::Schema.define(version: 2020_04_22_210707) do
     t.string "email"
     t.string "phone"
     t.text "message"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "customers", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
-    t.string "postalcode"
-    t.string "city"
-    t.string "province"
-    t.string "country"
-    t.string "email"
-    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
