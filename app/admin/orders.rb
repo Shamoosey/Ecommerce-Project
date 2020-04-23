369d9c9f559917c81ed367e6fba4bb774bc71031
Shamus Osler <Shamuso23@gmail.com>
2020-04-23 13:10:27 -0500

    removed quantity from orders

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register Order do
-  permit_params :customer_id, :totalamount, :subtotal, :gst, :pst, :quantity
+  permit_params :customer_id, :totalamount, :subtotal, :gst, :pst
 end
